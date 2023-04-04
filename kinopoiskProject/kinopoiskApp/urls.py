f5a82fea30494514912e0abe9a645c595bb387b5
unknown <yato.marville@mail.ru>
2023-04-04 22:58:53 +0600

    first version

---
+++ kinopoiskProject/kinopoiskApp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns =  [
+    path('', views.homePage, name='homePage'),
+    path('user/sign-up/', views.signUpPage, name='signUpPage'),
+    path('user/login/', views.loginPage, name='loginPage'),
+    path('user/logout/', views.logoutUser, name='logoutUser'),
+    path('movies/all/', views.allMoviesPage, name='allMoviesPage'),
+    path('movies/all/<slug:slug>', views.allMoviesPage, name='filterMoviesPage'),
+    path('movies/top/', views.topMoviesPage, name='topMoviesPage'),
+    path('movies/detail/<int:pk>/', views.movieDetailPage, name='movieDetailPage')
+]
